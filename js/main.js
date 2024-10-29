22920a36dbbb88e90b2eee5590e1e47f27c94d38
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-29 15:10:10 +0100

    add generate html card - js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,80 @@
-console.log("JS OK");
+//# Esercizio: Our Team
+// Dato un array di oggetti rappresentante un team di un’azienda,
+// creare una pagina dedicata in cui mostrare una card per ciascun componente.
+
+// BONUS
+// Rendere l’esercizio responsive, mandando a capo le card
+// Aggiungere un form di agginta membri che permetta di visualizzare il nuovo membro sulla pagina
+// (usate una foto qualunque)
+
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png",
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png",
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png",
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png",
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png",
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png",
+  },
+];
+
+//# Genero html card
+
+const cardsGrid = document.getElementById("cards-grid");
+console.log(cardsGrid);
+
+const generateCardTeamHTML = (fullname, role, email, image) => {
+  return;
+  ` <div class="col">
+        <div class="card mb-3 shadow-sm">
+            <div class="row g-0">
+            <div class="col-md-4">
+                <img
+                src="./img/female1.png"
+                class="img-fluid rounded-start"
+                alt="female1"
+                />
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                <h5 class="card-title">MARCO BIANCHI</h5>
+                <p class="card-text">Designer</p>
+                <p class="card-text">
+                    <small class="text-body-secondary"
+                    >maurobianchi@team.com</small
+                    >
+                </p>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>`;
+};
